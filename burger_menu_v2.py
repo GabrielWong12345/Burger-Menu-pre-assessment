1e0c914f9e5cc8bff6ddb94cc8b226dcbedd14a4
GabrielWong12345 <wongg2@middleton.school.nz>
2026-04-09 20:39:37 +1200

    v2

---
+++ burger_menu_v2.py
@@ -0,0 +1,53 @@
+"""
+Burger Combo Menu 
+v2 - Main Menu Loop
+Created by Gabriel Wong
+"""
+
+import easygui
+
+def setup_menu():
+    # [Combo name] : [Price, "Description of items in the combo"]
+    burger_menu = {
+        "Value": [10.50, "Beef burger, Fries, Fizzy drink"],
+        "Cheezy": [11.50, "Cheeseburger, Fries, Fizzy drink"],
+        "Super": [15.00, "Cheeseburger, Large fries, Smoothie"]
+    }
+    return burger_menu
+
+# MAIN LOOP
+if __name__ == "__main__":
+    current_menu = setup_menu()
+    
+    # Component  2: Main Menu Loop
+    run_program = True
+    while run_program:
+        
+        msg = "Welcome to the Burger Combo System. Please select an option:"
+        title = "Main Menu"
+        choices = ["View Menu", "Add Combo", "Search/Edit", "Delete", "Output All", "Exit"]
+        
+        selection = easygui.buttonbox(msg, title, choices)
+
+        if selection == "Exit" or selection is None:
+            easygui.msgbox("Program closing. Goodbye!")
+            run_program = False
+        
+        elif selection == "View Menu":
+            easygui.msgbox("You selected: View Menu")
+            
+        elif selection == "Add Combo":
+            easygui.msgbox("You selected: Add Combo")
+
+        elif selection == "Search/Edit":
+            easygui.msgbox("You selected: Search/Edit")
+
+        elif selection == "Delete":
+            easygui.msgbox("You selected: Delete ")
+
+        elif selection == "Output All":
+            # Just a placeholder for now to show it works
+            print("\n--- Current Menu ---")
+            for name, details in current_menu.items():
+                print(f"{name}: {details[1]} - ${details[0]:.2f}")
+            easygui.msgbox("Full menu printed to the console.")
