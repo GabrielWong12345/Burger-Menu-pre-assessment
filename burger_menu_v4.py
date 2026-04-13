97f1dd7a16db92f17e5af5b75512cadc670329a2
GabrielWong12345 <wongg2@middleton.school.nz>
2026-04-13 21:45:53 +1200

    v4

---
+++ burger_menu_v4.py
@@ -0,0 +1,70 @@
+"""
+Burger Combo Menu 
+v4 - Add Combo
+Created by Gabriel Wong
+"""
+
+import easygui
+
+def setup_menu():
+    # [Combo name] : [Price, "Description of items in the combo"]
+    burger_menu = {
+        "Value": [10.50, "Beef burger, Fries, Fizzy drink"],
+        "Cheezy": [11.50, "Cheeseburger, Fries, Fizzy drink"],
+        "Super": [15.00, "Cheeseburger, Large fries, Smoothie"]
+    }
+    return burger_menu
+
+def display_menu(menu):
+    """Formats the dictionary into a readable string for EasyGui"""
+    menu_string = ""
+    for name, details in menu.items():
+        menu_string += f"COMBO: {name.upper()}\nItems: {details[1]}\nPrice: ${details[0]:.2f}\n\n"
+    
+    easygui.msgbox(menu_string, "Current Burger Menu")
+
+# MAIN LOOP
+if __name__ == "__main__":
+    current_menu = setup_menu()
+    
+    # Component  2: Main Menu Loop
+    run_program = True
+    while run_program:
+        
+        msg = "Welcome to the Burger Combo System. Please select an option:"
+        title = "Main Menu"
+        choices = ["View Menu", "Add Combo", "Search/Edit", "Delete", "Output All", "Exit"]
+        
+        selection = easygui.buttonbox(msg, title, choices)
+
+        if selection == "Exit" or selection is None:
+            easygui.msgbox("Program closing. Goodbye!")
+            run_program = False
+        
+        elif selection == "View Menu":
+            display_menu(current_menu)
+        # Component 4: Add Combo
+        elif selection == "Add Combo":
+            new_name = easygui.enterbox("Enter the name of the new combo:", "Add Combo")
+            
+            if new_name:
+                new_items = easygui.enterbox(f"Enter the items for {new_name}:", "Add Items")
+                
+                try:
+                    new_price = float(easygui.enterbox(f"Enter the price for {new_name}:", "Add Price"))
+                    current_menu[new_name] = [new_price, new_items]
+                    easygui.msgbox(f"{new_name} has been added.")
+                except:
+                    easygui.msgbox("Invalid price. Combo not added.")
+
+        elif selection == "Search/Edit":
+            easygui.msgbox("You selected: Search/Edit")
+
+        elif selection == "Delete":
+            easygui.msgbox("You selected: Delete ")
+
+        elif selection == "Output All":
+            print("\n--- Current Menu ---")
+            for name, details in current_menu.items():
+                print(f"{name}: {details[1]} - ${details[0]:.2f}")
+            easygui.msgbox("Full menu printed to the console.")
